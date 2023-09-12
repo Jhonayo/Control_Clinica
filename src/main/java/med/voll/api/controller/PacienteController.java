130b952c9f04629d2d4bedc08211d8533dec544b
Jhonayo <j.iramirezpavez@gmail.com>
2023-09-12 13:07:43 -0300

    Incorporacion de tratamiento de errores

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,8 +1,8 @@
 package med.voll.api.controller;
 
-import med.voll.api.paciente.DatosListadoPaciente;
-import med.voll.api.paciente.DatosRegistroPaciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.domain.paciente.DatosListadoPaciente;
+import med.voll.api.domain.paciente.DatosRegistroPaciente;
+import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
